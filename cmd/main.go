37341f085a20b1d9ace13bd4999a58e90ace9f1e
Steffen Gebert <steffen.gebert@emnify.com>
2025-11-03 19:47:11 +0100

    Try AWS SDK directly

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,8 @@ import (
 
 	"log"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/sts"
 	"github.com/grafana/grafana-aws-sdk/pkg/awsauth"
 )
@@ -28,12 +30,30 @@ func Execute() {
 	creds, err := cfg.Credentials.Retrieve(ctx)
 	log.Printf("AWS config worked. Region: %v. AccessKey: %v. CredsSource: %v", cfg.Region, creds.AccessKeyID, creds.Source)
 
-	stsClient := sts.NewFromConfig(cfg)
+	if err = tryStsGetCallerIdentity(ctx, cfg); err != nil {
+		log.Fatalf("Failed to get sts caller identity: %v", err)
+	}
 
+	log.Printf("Trying regular AWS SDK...")
+
+	cfg, err = config.LoadDefaultConfig(context.TODO(), config.WithRegion("eu-west-1"))
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+
+	if err = tryStsGetCallerIdentity(ctx, cfg); err != nil {
+		log.Fatalf("Failed to get sts caller identity: %v", err)
+	}
+
+}
+
+func tryStsGetCallerIdentity(ctx context.Context, cfg aws.Config) error {
+	stsClient := sts.NewFromConfig(cfg)
 	callerId, err := stsClient.GetCallerIdentity(ctx, &sts.GetCallerIdentityInput{})
 	if err != nil {
 		log.Fatalf("Failed to get caller identity: %v", err)
-		return
+		return err
 	}
 	log.Printf("CallerId: %v", *callerId.Arn)
+	return nil
 }
