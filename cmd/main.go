9a07dc012a1a75c8678703005ad1ea04427a195e
Steffen Gebert <steffen.gebert@emnify.com>
2025-11-03 19:54:52 +0100

    Try AWS SDK first

--- cmd/main.go
+++ cmd/main.go
@@ -14,6 +14,19 @@ import (
 func Execute() {
 	ctx := context.Context(context.Background())
 
+	log.Printf("Trying regular AWS SDK...")
+
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("eu-west-1"))
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+
+	if err = tryStsGetCallerIdentity(ctx, cfg); err != nil {
+		log.Fatalf("Failed to get sts caller identity: %v", err)
+	}
+
+	log.Printf("Trying Grafana...")
+
 	authConfig := awsauth.NewConfigProvider()
 
 	authSettings := awsauth.Settings{
@@ -21,7 +34,7 @@ func Execute() {
 		CredentialsProfile: "default",
 	}
 
-	cfg, err := authConfig.GetConfig(ctx, authSettings)
+	cfg, err = authConfig.GetConfig(ctx, authSettings)
 	if err != nil {
 		log.Fatalf("Failed to get AWS config: %v", err)
 		return
@@ -29,17 +42,6 @@ func Execute() {
 
 	creds, err := cfg.Credentials.Retrieve(ctx)
 	log.Printf("AWS config worked. Region: %v. AccessKey: %v. CredsSource: %v", cfg.Region, creds.AccessKeyID, creds.Source)
-
-	if err = tryStsGetCallerIdentity(ctx, cfg); err != nil {
-		log.Fatalf("Failed to get sts caller identity: %v", err)
-	}
-
-	log.Printf("Trying regular AWS SDK...")
-
-	cfg, err = config.LoadDefaultConfig(context.TODO(), config.WithRegion("eu-west-1"))
-	if err != nil {
-		log.Fatalf("unable to load SDK config, %v", err)
-	}
 
 	if err = tryStsGetCallerIdentity(ctx, cfg); err != nil {
 		log.Fatalf("Failed to get sts caller identity: %v", err)
